f72657498fdcc4fdd00a7df939f1ab48940327d2
MayhemPipeworks <eatingabiscuit@gmail.com>
2020-06-19 17:37:36 +0100

    outside and traffic

---
+++ BikeMenu UnityProj/BikeMenu UnityProj/VehicleDriver.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class VehicleDriver : MonoBehaviour
+{
+    public float speed;
+
+    public Transform[] path;
+    int targetPoint;
+
+    void OnEnable()
+    {
+        targetPoint = 1;
+    }
+
+    void Update()
+    {
+        if (transform.position == path[targetPoint].position)
+        {
+            if (targetPoint == path.Length - 1)
+                Destroy(gameObject);
+
+            targetPoint++;
+        }
+        else
+        {
+            transform.position = Vector3.MoveTowards(transform.position, path[targetPoint].position, speed);
+            transform.LookAt(path[targetPoint], Vector3.up);
+        }
+    }
+}
---
+++ BikeMenu UnityProj/BikeMenu UnityProj/VehicleSpawner.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class VehicleSpawner : MonoBehaviour
+{
+    public GameObject[] vehicles;
+
+    public float vehicleSpeed;
+
+    public Transform[] pathToFollow;
+
+    float timeTillSpawn;
+
+    void Update()
+    {
+        if (Time.time >= timeTillSpawn)
+        {
+            SpawnVehicle();
+            timeTillSpawn = Time.time + UnityEngine.Random.Range(3, 8);
+        }
+    }
+
+    void SpawnVehicle()
+    {
+        GameObject car = Instantiate(vehicles[UnityEngine.Random.Range(0, vehicles.Length)], pathToFollow[0].position, pathToFollow[0].rotation);
+
+        VehicleDriver vd = car.AddComponent<VehicleDriver>();
+        vd.speed = vehicleSpeed;
+        vd.path = pathToFollow;
+    }
+}
